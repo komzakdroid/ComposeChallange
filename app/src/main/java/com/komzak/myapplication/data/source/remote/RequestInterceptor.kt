b1ff3857e0084d688a80feb735e7e51e735d29c1
komzakdroid <humotechgroup@gmail.com>
2023-08-31 17:07:25 +0500

    New Project

---
+++ app/src/main/java/com/komzak/myapplication/data/source/remote/RequestInterceptor.kt
@@ -0,0 +1,26 @@
+package com.komzak.myapplication.data.source.remote
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+/**
+ * Created by Komiljon Zokirov on 8/31/2023.
+ */
+class RequestInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val newUrl = originalRequest.url
+            .newBuilder()
+            .addQueryParameter(
+                "api_key",
+                "b251f83131ad1da248a073f4e86fd0dc"    //TODO Use your api key this one invalid
+            )
+            .build()
+        val request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+        return chain.proceed(request)
+    }
+}
---
+++ app/src/main/java/com/komzak/myapplication/data/source/remote/dto/Genre.kt
@@ -0,0 +1,13 @@
+package com.komzak.myapplication.data.source.remote.dto
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Genre(
+    @SerialName("id")
+    val id: Int?,
+    @SerialName("name")
+    val name: String?
+)
